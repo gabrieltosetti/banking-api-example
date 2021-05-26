bfeadfb52045bf4bff584c98be3b8430147a44bd
Gabriel Tosetti <tosetti.gabriel@gmail.com>
2021-05-26 20:56:56 -0300

    feat: set default currency log

--- utils/UserBankManager.php
+++ utils/UserBankManager.php
@@ -76,7 +76,10 @@ class UserBankManager
         }
 
         if ($this->bankAccount->balance < $convertedValue) {
-            $balance = $this->bankAccount->balance;
+            $code = $this->bankAccount->currency->code;
+            $convertedValue = '$' . number_format($convertedValue, 2, ',', '.') . " $code";
+            $balance = '$' . number_format($this->bankAccount->balance, 2, ',', '.') . " $code";
+
             throw new \Exception(
                 "Withdraw value ($convertedValue) is greater than your account balance ($balance)."
             );
@@ -96,18 +99,30 @@ class UserBankManager
         return;
     }
 
-    public function setDefaultCurrency(Currency $currency)
+    public function setDefaultCurrency(Currency $targetCurrency)
     {
+        $oldCurrencyId = $this->bankAccount->currency_id;
+        $oldAccountBalance = $this->bankAccount->balance;
+        $newCurrencyId = $targetCurrency->currency_id;
+
         $exchange = $this->currencyExchanger->convert(
             $this->bankAccount->currency,
-            $currency,
+            $targetCurrency,
             $this->bankAccount->balance
         );
 
-        $this->bankAccount->currency_id = $currency->id;
+        $this->bankAccount->currency_id = $targetCurrency->id;
         $this->bankAccount->balance = $exchange['value'];
         $this->bankAccount->save();
 
         $this->bankAccount->currency->refresh();
+
+        $transaction = new Transaction();
+        $transaction->bank_account_id = $this->bankAccount->id;
+        $transaction->bank_account_currency_id = $oldCurrencyId;
+        $transaction->target_currency_id = $newCurrencyId;
+        $transaction->value = $oldAccountBalance;
+        $transaction->rate = $exchange['rate'];
+        $transaction->save();
     }
 }
